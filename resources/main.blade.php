1e9c5d8df338480039ff70500df96c0b7c19c5a2
lukevictor <lucas.c.victor@hotmail.com>
2017-06-12 15:13:23 -0300

    Criação das views principais

---
+++ resources/main.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+
+    @include('partials._head')
+    @yield('extrastyle')
+
+  </head>
+
+    <body style="background-color: #dbe3e9" id="app-layout">
+
+      @include('partials._nav')
+
+        <div  class="container-fluid" style="padding-top: 22%;">
+          
+          <div class="row">
+
+              @yield('conteudo')
+
+          </div>
+           
+
+            @include('partials._footer')
+
+        </div>
+
+      @include('partials._javascript')
+      @yield('extrascript')
+
+
+    </body>
+  
+
+</html>
